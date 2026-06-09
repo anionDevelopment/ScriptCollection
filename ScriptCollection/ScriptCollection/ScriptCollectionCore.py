55e7eb5106e5f140740e326311e54f83530f34ab
Marius Göcke <marius.goecke@gmail.com>
2026-06-10 01:05:14 +0200

    added changelog

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -38,7 +38,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.2.131"
+version = "4.2.132"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
