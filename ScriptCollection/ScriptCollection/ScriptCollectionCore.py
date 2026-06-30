858332695491a7235d40e2c86ce85ce36423c62f
Marius Göcke <marius.goecke@gmail.com>
2026-06-30 22:27:58 +0200

    Pre-merge-commit

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -37,7 +37,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.3.13"
+version = "4.3.14"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
