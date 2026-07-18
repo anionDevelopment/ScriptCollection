b65d2711e5101203718bff79485d09435107d4cd
Marius Göcke <marius.goecke@gmail.com>
2026-07-18 18:22:05 +0200

    replaced gitversion usage for calculating version of codeunits

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -38,7 +38,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.3.37"
+version = "4.3.38"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
