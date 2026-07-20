c80fc512eaaa8e1f19d4ba9d551a8c194fbfd145
Marius Göcke <marius.goecke@gmail.com>
2026-07-20 13:04:00 +0200

    added merge flag

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -39,7 +39,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.3.46"
+version = "4.3.47"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
