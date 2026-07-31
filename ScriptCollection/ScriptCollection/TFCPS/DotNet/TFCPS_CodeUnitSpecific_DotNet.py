a4e1c22affb5d97ca7f967aac164a228eda0a957
Marius Göcke <marius.goecke@gmail.com>
2026-07-31 13:14:30 +0200

    updated timeout for .net tests to 45min

--- ScriptCollection/ScriptCollection/TFCPS/DotNet/TFCPS_CodeUnitSpecific_DotNet.py
+++ ScriptCollection/ScriptCollection/TFCPS/DotNet/TFCPS_CodeUnitSpecific_DotNet.py
@@ -620,7 +620,7 @@ class TFCPS_CodeUnitSpecific_DotNet_Functions(TFCPS_CodeUnitSpecific_Base):
         test_working_directory = os.path.join(temp_folder, "WorkingDirectory")
         GeneralUtilities.ensure_directory_exists(test_working_directory)
         try:
-            program_output=self._protected_sc.run_program_argsasarray("dotnet", args, test_working_directory, print_live_output=self.get_verbosity()==LogLevel.Debug, timeoutInSeconds=60*20, env_vars={"MSBUILDDISABLENODEREUSE": "1"})
+            program_output=self._protected_sc.run_program_argsasarray("dotnet", args, test_working_directory, print_live_output=self.get_verbosity()==LogLevel.Debug, timeoutInSeconds=60*45, env_vars={"MSBUILDDISABLENODEREUSE": "1"})
             test_output:str=program_output[1]
             output_lines=program_output[1].split("\n")
             output_lines=[line for line in output_lines if GeneralUtilities.string_has_content(line)]
