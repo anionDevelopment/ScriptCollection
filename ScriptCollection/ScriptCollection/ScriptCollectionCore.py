c38388d5b0c6fc2cbe15da83a7ecaa5041d0c8bb
Marius Göcke <marius.goecke@gmail.com>
2026-08-21 16:43:04 +0200

    fix for .net projects

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -39,7 +39,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.4.10"
+version = "4.4.11"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
