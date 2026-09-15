74a1e81cf16ed3f365dda142c7e886113f6ef9e5
Marius Göcke <marius.goecke@gmail.com>
2026-09-15 16:37:50 +0200

    Saved updates before creating pull-request for branch 'other/maintenance' of repository 'ScriptCollection'.

--- ScriptCollection/ScriptCollection/ScriptCollectionCore.py
+++ ScriptCollection/ScriptCollection/ScriptCollectionCore.py
@@ -40,7 +40,7 @@ from .ProgramRunnerBase import ProgramRunnerBase
 from .ProgramRunnerPopen import ProgramRunnerPopen
 from .SCLog import SCLog, LogLevel
 
-version = "4.4.23"
+version = "4.4.24"
 __version__ = version
 
 class VSCodeWorkspaceShellTask:
